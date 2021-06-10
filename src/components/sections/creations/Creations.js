064ea709bd5a080a35b7b2e37f2980f2f462fa0a
MauroPettinari <mauroapettinari@gmail.com>
2021-06-09 21:34:01 -0300

    Initial project structure

---
+++ src/components/sections/creations/Creations.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Creations = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/components/sections/creations/CreationsCard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const CreationsCard = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
